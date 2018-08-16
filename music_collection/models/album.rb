2192f74573eca389a3284f6c6d1f71d70d6f88af
GerCassa <gercassa@gmail.com>
2018-08-16 09:31:03 +0100

    last commit to submit homework

--- music_collection/models/album.rb
+++ music_collection/models/album.rb
@@ -22,19 +22,42 @@ class Album
     @id = SqlRunner.run(sql, values)[0]['id'].to_i
   end
 
+  def update()
+    sql = "UPDATE albums
+    SET
+    (name, genre, artist_id)
+    =
+    ($1, $2, $3)
+    WHERE id = $4"
+    values = [@name, @genre, @artist_id, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def artist()
+    sql = "SELECT * FROM artists WHERE id = $1"
+    values = [@artist_id]
+    artists = SqlRunner.run(sql, values)
+    return Artist.new(artists[0])
+  end
+
+  def delete()
+    sql = "DELETE FROM albums WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def Album.find_album_by_id(id)
+    sql = "SELECT * FROM albums WHERE id = $1"
+    values = [id]
+    albums = SqlRunner.run(sql, values)
+    return albums.map {|album| Album.new(album)}[0]
+  end
+
   def Album.all()
     sql = "SELECT * FROM albums"
     albums = SqlRunner.run(sql)
     return albums.map { |album| Album.new(album) }
   end
-
-  def by_artist()
-    sql = "SELECT * FROM albums WHERE artist_id = $1"
-    values = [@artist_id]
-    by_art = SqlRunner.run(sql, values)
-    return by_art.map {|album| Album.new(album)}
-  end
-
 
   def Album.delete_all()
     sql = "DELETE FROM albums"
--- music_collection/models/artist.rb
+++ music_collection/models/artist.rb
@@ -20,17 +20,47 @@ class Artist
     @id = SqlRunner.run(sql, values)[0]['id'].to_i
   end
 
+  def albums()
+    sql = "SELECT * FROM albums WHERE artist_id = $1"
+    values = [@id]
+    by_art = SqlRunner.run(sql, values)
+    return by_art.map {|album| Album.new(album)}
+  end
+
+  def update()
+    sql = "UPDATE albums
+    SET
+    (name, genre, artist_id)
+    =
+    ($1, $2, $3)
+    WHERE id = $4"
+    values = [@name, @genre, @artist_id, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM artists WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def Artist.find_artist_by_id(id)
+    sql = "SELECT * FROM artists WHERE id = $1"
+    values = [id]
+    artists = SqlRunner.run(sql, values)
+    return artists.map {|artist| Artist.new(artist)}[0]
+  end
+
   def Artist.all()
     sql = "SELECT * FROM artists"
     artists = SqlRunner.run(sql)
     return artists.map { |artist| Artist.new(artist) }
   end
 
-    def Artist.delete_all()
-      sql = "DELETE FROM artists"
-      SqlRunner.run(sql)
-    end
-
+  def Artist.delete_all()
+    sql = "DELETE FROM artists"
+    SqlRunner.run(sql)
+  end
 
 
 end
